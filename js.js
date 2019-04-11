4325680dab1955e3829d4cc99f62dce7d409d7f1
LissetteAcv2 <47862635+LissetteAcv2@users.noreply.github.com>
2019-04-11 12:05:57 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+Function enviar(){
+Var usuario= document.getElementById (¨user¨).value;
+var contraseña= document.getElementById (¨pass¨).value;
+If (Usuario=="sergio") (contraseña=="123"){
+    alert("Hola mundo");
+ } else {
+     Alert (intentalo de nuevo)
+ }
